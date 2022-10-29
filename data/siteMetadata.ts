e7eeecbd96f075a6793c74dc1d6a1197e610c616
wqz66 <Xxbys752521@gmail.com>
2022-10-29 13:41:18 +0800

    init commit

---
+++ data/siteMetadata.ts
@@ -0,0 +1,45 @@
+const siteMetadata = {
+  title: "Xxbys",
+  author: "QizhaoWang",
+  SEO_author: {
+    first_name: "Qizhao",
+    last_name: "Wang",
+    username: "Xxbys",
+    gender: "male",
+  },
+  headerTitle: "Xxbys",
+  description: "A place to put my thoughts in writing",
+  siteRepo: "https://github.com/Xxbys752521/XxbysPersonalSite",
+  siteLogo: "/static/images/logo.png",
+  image: "/static/images/avatar.png",
+  socialBanner: "/static/images/socialbg.png",
+
+  email: "Xxbys752521@Gmail.com",
+  github: "https://github.com/th1nhng0",
+
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports plausible, simpleAnalytics, umami or googleAnalytics
+    plausibleDataDomain: "", // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    umamiWebsiteId: "", // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: "", // e.g. UA-000000-2 or G-XXXXXXX
+    posthogAnalyticsId: "", // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
+  },
+  comment: {
+    giscusConfig: {
+      repo: "Th1nhNg0/th1nhng0.vercel.app",
+      repositoryId: "MDEwOlJlcG9zaXRvcnk0MDI2MDQ1MDE=",
+      category: "General",
+      categoryId: "DIC_kwDOF_9B1c4B-6vK",
+      mapping: "pathname",
+      reactions: "1",
+      metadata: "0",
+      inputPosition: "top",
+      lang: "en",
+    },
+  },
+};
+
+export default siteMetadata;
